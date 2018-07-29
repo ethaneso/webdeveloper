8658006059fd6ac6d78084df04e2af93a915b4e0
ethankclam <ethankclam@gmail.com>
2018-07-29 19:38:17 +0800

    loginsystem

---
+++ registry/includes/dbh.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbServername = "shareddb-h.hosting.stackcp.net";
+$dbUsername = "registry-33310620";
+$dbPassword = "registry20180729";
+$dbName = "registry-33310620";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName)
+
+?>
