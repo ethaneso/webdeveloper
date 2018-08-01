12ed6b401a9b05ec8d8157400980cdbf035e587e
ethankclam <ethankclam@gmail.com>
2018-08-01 23:43:29 +0800

    to do app

---
+++ todolist/js/main.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+
+
+// user clicked on the add button
+document.getElementById('add').addEventListener('click', function() {
+	var value = document.getElementById('item').value;
+
+	console.log(value);
+});
+
+	
+
+}
